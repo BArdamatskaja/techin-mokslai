60af83afd08c6fca11ea0659be787c1f219b9886
Bozena <malachovskaja123@gmail.com>
2025-06-04 23:52:44 +0300

    Add 17.2-1 and 17.2-2 folders

---
+++ tinklalapiu_kurimas/javascript/3_pamoka/17.2/1/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// function dayTime(paruSk) {
+//  const h = paruSk * 24;
+//  const min = h * 60;
+//  return { h, min, paruSk };
+// }
+
+// function dayTime(paruSk) {
+//  return paruSk;
+// }
+// const h = paruSk * 24;
+// const min = h * 60;
+
+const paros = dayTime(5);
+console.log(
+  paros + " dienos yra " + h + " valandos, " + " arba " + min + " minučių."
+);
+// 2 bandyti variantai, nesupratau kodel neveikia.
---
+++ tinklalapiu_kurimas/javascript/3_pamoka/17.2/2/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function reiksme(x) {
+  let y = 16 * Math.pow(x, 4) + 2 * x;
+  return y;
+}
+const yReiksme = reiksme(5);
+console.log(yReiksme);
