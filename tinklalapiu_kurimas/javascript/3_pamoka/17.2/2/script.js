7eb52e6fc095cffb4440a21a50e554fed1919540
Bozena <malachovskaja123@gmail.com>
2025-06-04 23:56:42 +0300

    Update script.js file

--- tinklalapiu_kurimas/javascript/3_pamoka/17.2/2/script.js
+++ tinklalapiu_kurimas/javascript/3_pamoka/17.2/2/script.js
@@ -4,5 +4,6 @@ function reiksme(x) {
   let y = 16 * Math.pow(x, 4) + 2 * x;
   return y;
 }
-const yReiksme = reiksme(5);
+let x = Number(prompt("x reikšmė:"));
+const yReiksme = reiksme(x);
 console.log(yReiksme);
